c4c496c02fc40613f3a72c98db3adb1c86bab9a2
GMG-Developer <bayuindra091191@gmail.com>
2018-01-19 17:04:28 +0700

    Employee in progress

---
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: GMG-Developer
+ * Date: 19/01/2018
+ * Time: 14:19
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class EmployeeController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    public function edit(){
+
+    }
+
+    public function update(){
+
+    }
+}
