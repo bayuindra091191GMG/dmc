3540f64642f5fdce41df8d35302b03ecebcfcb4c
GMG-Developer <bayuindra091191@gmail.com>
2018-01-22 17:07:21 +0700

    Employee in progress

--- webpack.mix.js
+++ webpack.mix.js
@@ -138,7 +138,7 @@ mix.styles([
     'resources/assets/admin/css/dashboard.css',
 ], 'public/assets/admin/css/dashboard.css').version();
 
-// DataTables
+// USER
 mix.scripts([
     'node_modules/datatables.net/js/jquery.dataTables.js',
 ], 'public/assets/admin/js/users/index.js').version();
@@ -146,6 +146,16 @@ mix.scripts([
 mix.styles([
     'resources/assets/admin/css/jquery.datatables.min.css',
 ], 'public/assets/admin/css/users/index.css').version();
+
+// EMPLOYEE
+mix.scripts([
+    'node_modules/gentelella/vendors/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
+    'resources/assets/admin/js/employee/edit.js'
+], 'public/assets/admin/js/employee/edit.js').version();
+
+mix.styles([
+    'C:/PHP Project/victor23mega/node_modules/gentelella/vendors/bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css',
+], 'public/assets/admin/css/employee/edit.css').version();
 
 
 /*
